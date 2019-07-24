83f8b6ca4ad9d8dade6718eaa20553ec403dd9c3
Travis CI User <travis@example.org>
2019-07-24 18:36:01 +0000

    Deploy to GitHub Pages master branch: 5614d7961e565fe18d4d264cbc9ad7b48faf785d

--- static/jailed/jailed.js
+++ static/jailed/jailed.js
@@ -37,9 +37,13 @@ if (__is__node__) {
   __jailed__path__ = __dirname + "/";
 } else {
   // web
-  __jailed__path__ = `${location.protocol}//${location.hostname}${
-    location.port ? ":" + location.port : ""
-  }/static/jailed/`;
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    __jailed__path__ = `${location.protocol}//${location.hostname}${
+      location.port ? ":" + location.port : ""
+    }/static/jailed/`;
+  } else {
+    __jailed__path__ = "//lib.imjoy.io/static/jailed/";
+  }
   // var scripts = document.getElementsByTagName("script");
   // __jailed__path__ = scripts[scripts.length-1].src
   //     .split('?')[0]
@@ -122,6 +126,14 @@ Whenable.prototype._checkHandler = function(handler) {
 };
 
 /**
+ * Initializes the library site for Node.js environment (loads
+ * _JailedSite.js)
+ */
+var initNode = function() {
+  require("./_JailedSite.js");
+};
+
+/**
  * Initializes the library site for web environment (loads
  * _JailedSite.js)
  */
@@ -168,12 +180,129 @@ var initWeb = function() {
 };
 
 var BasicConnection;
+
+/**
+ * Creates the platform-dependent BasicConnection object in the
+ * Node.js environment
+ */
+var basicConnectionNode = function() {
+  var childProcess = require("child_process");
+
+  /**
+   * Platform-dependent implementation of the BasicConnection
+   * object, initializes the plugin site and provides the basic
+   * messaging-based connection with it
+   *
+   * For Node.js the plugin is created as a forked process
+   */
+  BasicConnection = function(id, type) {
+    if (type == "iframe") {
+      throw "You can not use iframe in nodejs.";
+    }
+    this.id = id;
+    // in Node.js always has a subprocess
+    this.dedicatedThread = true;
+    this._disconnected = false;
+    this._messageHandler = function() {};
+    this._disconnectHandler = function() {};
+
+    this._process = childProcess.fork(__jailed__path__ + "_pluginNode.js");
+
+    var me = this;
+    this._process.on("message", function(m) {
+      me._messageHandler(m);
+    });
+
+    this._process.on("exit", function(m) {
+      me._disconnected = true;
+      me._disconnectHandler(m);
+    });
+  };
+
+  /**
+   * Sets-up the handler to be called upon the BasicConnection
+   * initialization is completed.
+   *
+   * For Node.js the connection is fully initialized within the
+   * constructor, so simply calls the provided handler.
+   *
+   * @param {Function} handler to be called upon connection init
+   */
+  BasicConnection.prototype.whenInit = function(handler) {
+    handler();
+  };
+
+  /**
+   * Sets-up the handler to be called upon the BasicConnection
+   * failed.
+   *
+   * For Node.js the connection is fully initialized within the
+   * constructor, so simply calls the provided handler.
+   *
+   * @param {Function} handler to be called upon connection init
+   */
+  BasicConnection.prototype.whenFailed = function(handler) {
+    handler();
+  };
+
+  /**
+   * Sends a message to the plugin site
+   *
+   * @param {Object} data to send
+   */
+  BasicConnection.prototype.send = function(data, transferables) {
+    if (!this._disconnected) {
+      this._process.send(data, transferables);
+    }
+  };
+
+  /**
+   * Adds a handler for a message received from the plugin site
+   *
+   * @param {Function} handler to call upon a message
+   */
+  BasicConnection.prototype.onMessage = function(handler) {
+    this._messageHandler = function(data) {
+      // broken stack would break the IPC in Node.js
+      try {
+        handler(data);
+      } catch (e) {
+        console.error(e.stack);
+      }
+    };
+  };
+
+  /**
+   * Adds a handler for the event of plugin disconnection
+   * (= plugin process exit)
+   *
+   * @param {Function} handler to call upon a disconnect
+   */
+  BasicConnection.prototype.onDisconnect = function(handler) {
+    this._disconnectHandler = handler;
+  };
+
+  /**
+   * Disconnects the plugin (= kills the forked process)
+   */
+  BasicConnection.prototype.disconnect = function() {
+    this._process.kill("SIGKILL");
+    this._disconnected = true;
+  };
+};
+
 /**
  * Creates the platform-dependent BasicConnection object in the
  * web-browser environment
  */
 var basicConnectionWeb = function() {
-  var perm = ["allow-scripts", "allow-forms", "allow-same-origin"];
+  var perm = [
+    "allow-scripts",
+    "allow-forms",
+    "allow-same-origin",
+    "allow-modals",
+    "allow-popups",
+  ];
 
   if (__jailed__path__.substr(0, 7).toLowerCase() == "file://") {
     // local instance requires extra permission
@@ -184,7 +313,10 @@ var basicConnectionWeb = function() {
   var sample = document.createElement("iframe");
   sample.src = __jailed__path__ + "_frame.html";
   sample.sandbox = perm.join(" ");
-  sample.allow = "midi *; geolocation *; microphone *; camera *; encrypted-media *;";
+  sample.allow =
+    "midi *; geolocation *; microphone *; camera *; encrypted-media *;";
+  sample.allowfullscreen = "";
+  sample.allowpaymentrequest = "";
   sample.frameBorder = "0";
   sample.style.width = "100%";
   sample.style.height = "100%";
@@ -212,7 +344,13 @@ var basicConnectionWeb = function() {
       if (!me._disconnected) {
         me._frame = sample.cloneNode(false);
         me._frame.src =
-          me._frame.src + "?type=" + type + "&name=" + config.name;
+          me._frame.src +
+          "?type=" +
+          type +
+          "&name=" +
+          config.name +
+          "&workspace=" +
+          config.workspace;
         me._frame.id = "iframe_" + id;
         if (
           type == "iframe" ||
@@ -496,7 +634,9 @@ var SocketioConnectionWeb = function() {
 };
 
 if (__is__node__) {
-  throw "nodejs is not supported.";
+  initNode();
+  basicConnectionNode();
+  SocketioConnectionWeb();
 } else {
   initWeb();
   basicConnectionWeb();

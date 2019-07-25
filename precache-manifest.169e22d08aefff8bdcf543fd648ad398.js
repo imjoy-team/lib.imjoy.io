d22f622e5ca3de459966ab645c37c68fd3b0080e
Wei Ouyang <oeway007@gmail.com>
2019-07-25 14:48:40 +0200

    Create precache-manifest.169e22d08aefff8bdcf543fd648ad398.js

---
+++ precache-manifest.169e22d08aefff8bdcf543fd648ad398.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "875b5edc4f40c8874278d29f654cb4f4",
+    "url": "static/jailed/_JailedSite.js"
+  },
+  {
+    "revision": "f9bf1db80923c73a2a8aac899bae0c16",
+    "url": "joy.css"
+  },
+  {
+    "revision": "7745b7e6b22a5bddfc490eb9b4ba7e58",
+    "url": "static/jailed/jailed.js"
+  },
+  {
+    "revision": "37567270f075274d9df79918d238e499",
+    "url": "static/jailed/_frame.js"
+  },
+  {
+    "revision": "57b010e2ad23bba6a8b626b01a83bf4d",
+    "url": "static/jailed/_frame.html"
+  },
+  {
+    "revision": "3c947349d7fe3b84b65a0501ec4958f3",
+    "url": "static/jailed/_pluginCore.js"
+  },
+  {
+    "revision": "58c4c810c30ccedf55e748055b82db18",
+    "url": "static/jailed/_pluginWebWorker.js"
+  },
+  {
+    "revision": "04519ca456a2e9e97f36f65bed34f8d4",
+    "url": "static/jailed/_pluginNode.js"
+  },
+  {
+    "revision": "4f3865c867909dfdd715c51b98d787dd",
+    "url": "static/jailed/_pluginWebIframe.js"
+  },
+  {
+    "revision": "d7d11a6368ade0eadf7c9247a1bc2d1a",
+    "url": "static/jailed/_pluginWebPython.js"
+  },
+  {
+    "revision": "d961be179e269dc83608",
+    "url": "imjoy-lib.min.js"
+  },
+  {
+    "revision": "0acd51d81444ddeb54e82135aa4c6527",
+    "url": "imjoy-lib.html"
+  }
+];

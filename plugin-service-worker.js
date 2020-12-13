abef50e75443b94f79eff2ad9c69189a3fca2fd0
oeway <oeway@users.noreply.github.com>
2020-12-13 16:43:10 +0000

    Deployed from imjoy-team/imjoy-core@f4755f586509ee19c0948d3e315e9af556c563aa

--- plugin-service-worker.js
+++ plugin-service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.7e23b130727e0d7881ae5b1ab6d065a6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.d10d1c7b44cd4eeb310b3b25ba5d911d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable */
 if (typeof workbox !== "undefined") {
--- precache-manifest.d10d1c7b44cd4eeb310b3b25ba5d911d.js
+++ precache-manifest.d10d1c7b44cd4eeb310b3b25ba5d911d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "12e49423bdcf7ac2ba19",
+    "revision": "21f3fe0ed4a7f9a16959",
     "url": "imjoy-rpc.min.js"
   }
 ]);

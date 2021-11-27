05e368e968fee1abfa367e92210e5fcdb808c735
oeway <oeway007@gmail.com>
2021-11-27 19:09:15 +0100

    Manually deployed.

--- plugin-service-worker.js
+++ plugin-service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.d6a84d76721020932b268ecca3fb73d0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.15495a8c7553519de93e8b1c6bcaeada.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable */
 if (typeof workbox !== "undefined") {
--- precache-manifest.15495a8c7553519de93e8b1c6bcaeada.js
+++ precache-manifest.15495a8c7553519de93e8b1c6bcaeada.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a8d7f46951d196b86ab",
+    "revision": "251cfe753565e5d1d1f9",
     "url": "imjoy-rpc-socketio.min.js"
   },
   {
-    "revision": "386fad4af90bc0741fcb",
+    "revision": "042156b1ff8e2409a296",
     "url": "imjoy-rpc.min.js"
   }
 ]);
